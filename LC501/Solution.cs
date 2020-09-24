7b50f1d437a1009e98c13281c6932fe2241b4880
llibetter <915133364@qq.com>
2020-09-24 21:41:06 +0800

    .

--- LC501/Solution.cs
+++ LC501/Solution.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 public class TreeNode
 {
-    public int val;//
+    public int val;/////
     public TreeNode left;
     public TreeNode right;
     public TreeNode(int x) { val = x; }
