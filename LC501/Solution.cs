61480b36c58a6c8522fb970cee111f4ca08d3473
llibetter <915133364@qq.com>
2020-09-24 20:42:32 +0800

    second commit

---
+++ LC501/Solution.cs
@@ -0,0 +1,45 @@
+
+using System.Collections.Generic;
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
+
+public class Solution
+{
+    List<int> result = new List<int>();
+    int maxTime = 0;
+    int curTime = 0;
+    TreeNode pre = null;
+    public int[] FindMode(TreeNode root)
+    {
+        InOrder(root);
+        return result.ToArray();
+    }
+
+    public void InOrder(TreeNode root)
+    {
+        if (root == null) return;
+        InOrder(root.left);
+        if (pre == null || pre.val == root.val)
+            curTime++;
+        else
+            curTime = 1;
+
+        if (curTime == maxTime)
+        {
+            result.Add(root.val);
+        }
+        else if (curTime > maxTime)
+        {
+            result.Clear();
+            result.Add(root.val);
+            maxTime = curTime;
+        }
+        pre = root;
+        InOrder(root.right);
+    }
+}
