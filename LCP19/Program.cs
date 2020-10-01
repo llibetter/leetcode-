45dd63e53bdb5b52fd2554d606adda9252e0d7eb
llibetter <915133364@qq.com>
2020-10-01 20:35:06 +0800

    秋叶收藏集

---
+++ LCP19/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LCP19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
