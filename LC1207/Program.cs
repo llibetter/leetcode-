556e3c733367e7cd4d866a364a151530a720587c
llibetter <915133364@qq.com>
2020-10-28 21:00:45 +0800

    LC1207. 独一无二的出现次数

---
+++ LC1207/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LC1207
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool res=new Solution().UniqueOccurrences(new int[]{1,2,2,1,1,3});
+            Console.WriteLine(res);
+
+            Console.Read();
+        }
+    }
+}
---
+++ LC1207/Solution.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+/*
+1207. 独一无二的出现次数
+给你一个整数数组 arr，请你帮忙统计数组中每个数的出现次数。
+
+如果每个数的出现次数都是独一无二的，就返回 true；否则返回 false。
+
+ 
+
+示例 1：
+
+输入：arr = [1,2,2,1,1,3]
+输出：true
+解释：在该数组中，1 出现了 3 次，2 出现了 2 次，3 只出现了 1 次。没有两个数的出现次数相同。
+*/
+public class Solution {
+    public bool UniqueOccurrences(int[] arr) {
+        int[] cnt=new int[2005];
+        foreach(var item in arr)
+            cnt[item+1000]++;
+        HashSet<int> hs=new HashSet<int>();
+        for(int i=0;i<2005;i++)
+        {
+            if(cnt[i]==0) continue;
+            if(hs.Contains(cnt[i])) return false;
+            hs.Add(cnt[i]);
+        }
+        return true;
+    }
+}
