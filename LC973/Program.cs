372362e2a94f5741dede1749430b5d1f0737e270
llibetter <915133364@qq.com>
2020-11-09 20:10:24 +0800

    LC973. 最接近原点的 K 个点

---
+++ LC973/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LC973
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
